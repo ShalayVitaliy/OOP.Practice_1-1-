3a02a8408f0bd8672f6f63bdccd1a784dc516d19
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-08-15 15:38:01 +0300

    Created Owner

---
+++ Task_1/Owner.java
@@ -0,0 +1,14 @@
+package Task_1;
+
+public class Owner {
+    
+    private String owner_name;
+
+    public String getOwner_name() {
+        return owner_name;
+    }
+
+    public Owner(String owner_name){
+        this.owner_name = owner_name;
+    }
+}
